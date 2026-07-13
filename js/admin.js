fd98db22d9e2f3fc74a9231d71c3675e350305b7
chillzy998 <mj3918186@gmail.com>
2026-07-13 08:45:22 +0800

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -370,7 +370,6 @@ if (contentBtn) {
 // LOGOUT SYSTEM
 // ==========================================
 
-import { signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 
 const logoutBtn = document.getElementById("logout-btn");
