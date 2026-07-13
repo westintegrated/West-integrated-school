11b57a3cb7f045bace3e7f1a10e5956263231f3b
chillzy998 <mj3918186@gmail.com>
2026-07-13 09:34:49 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
 const firebaseConfig = {
 
@@ -12,15 +11,18 @@ const firebaseConfig = {
   messagingSenderId: "1085696949595",
   appId: "1:1085696949595:web:69c0e2f2e4ee28220dae7e",
   measurementId: "G-KWGLD5Z410"
-
 };
 
+
 const app = initializeApp(firebaseConfig);
+
 
 const db = getFirestore(app);
 
 const auth = getAuth(app);
 
-const storage = getStorage(app);
 
-export { db, auth, storage };
+export {
+    db,
+    auth
+};
